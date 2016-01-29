89538afc47e9b12c49e71b0eef6e86cd2ef3dc7a
Jon Wexler <jwexman@gmail.com>
2016-01-28 19:23:50 -0500

    changed and formatted to use images from DB

--- image-sharer.js
+++ image-sharer.js
@@ -1,48 +1,41 @@
-Images = new Mongo.Collection("images");
+Images = new Mongo.Collection("images"); 
 console.log(Images.find().count());
 
-// Is the code running in the client? the browser?
+// Is the code running in the client? the browser? 
 if (Meteor.isClient) {
+  Accounts.ui.config({ passwordSignupFields: "USERNAME_ONLY" });
 
-  Accounts.ui.config({
-    passwordSignupFields: "USERNAME_ONLY"
-  });
   console.log("Running in Client!");
 
-  var images = [
-  {
-    image_src: "guyf.gif",
-    image_alt: "Reverse Guy"
-  },
-  {
-    image_src: "alanb.gif",
-    image_alt: "Alan as Bieber"
-  },
-  {
-    image_src: "alan.gif",
-    image_alt: "Alan Gif"
-  }
-  ]
-  Template.body.helpers({
-    username: function(){
-      if (Meteor.user()) {
-        console.log(Meteor.user())
-        return Meteor.user().emails[0].address;
+  var set_images = [{image_src: "guyf.gif", image_alt: "Reverse Guy" },
+  {image_src: "alanb.gif", image_alt: "Alan as Bieber" }, { image_src:
+    "alan.gif", image_alt: "Alan Gif" } ] 
 
-      } else {
-        return "Jon";
+  var images = Images.find({})
 
-      };
-    }
-  })
+    Template.body.helpers({ 
+      username: function(){ 
+        if (Meteor.user()) { 
+          console.log(Meteor.user()) 
+          return Meteor.user().username.toUpperCase();
+        } else {   
+          return "World!";
 
-  Template.images.helpers ({data_images: images})
-  Template.time.helpers({
-    now: new Date()
-  })
+        };     
+      }   
+    })
+    Template.body.events({
+      "click .add-image" : function(){
+
+      }
+    })
+
+  Template.images.helpers ({data_images: images})   
+  
+  Template.time.helpers({now: new Date() }) 
 }
 
-// Is the code running in the server?
+// Is the code running in the server? 
 if (Meteor.isServer) {
-  console.log("Running in Server!");
+  console.log("Running in Server!"); 
 }
